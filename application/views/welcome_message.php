45842e8524a07611e0f566c410121a1c03ad3b4a
raflytamam371 <raflytamam42@gmail.com>
2024-03-21 22:05:26 +0700

    Tampil Biodata

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -79,18 +79,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Biodata!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Biodata Mahasiswa BSI Margonda.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p>Sebagai berikut :</p>
+		<code>
+			<p>NIM		: 1234567890</p>
+			<p>NAMA	: Yesa Dwi Aprilia</p>
+			<p>KELAS	: 15.4D.01</p>
+			<p>Prodi	: Ilmu Komputer</p>
+			<p>Tanggal Lahir	: </p>
+			<p>Tempat Lahir	: </p>
+			<p>Alamat	: </p>
+			<p>Jenis Kelamin	:</p>
+			<p>Agama	:</p>
+		</code>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
+		<p>Diatas merupakan contoh biodata sementara:</p>
+		<code>Terimakasih</code>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
