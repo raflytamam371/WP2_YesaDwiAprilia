d6e162b390cbbd02a5630360e04e5c895e946422
raflytamam371 <raflytamam42@gmail.com>
2024-03-21 21:29:26 +0700

    First Commit

---
+++ application/views/biodata.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BIODATAKU</h1>
+    <span>Nim :</span></br>
+    <span>Nama :</span></br>
+    <span>Kelas :</span></br>
+    <span>Matkul :</span></br>
+</body>
+</html>
